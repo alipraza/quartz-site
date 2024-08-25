3043c8fa9201f12f538a25cef88e638671095b6e
alipraza <123073798+alipraza@users.noreply.github.com>
2024-08-25 23:29:19 +0500

    Update configuration settings

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "quartz-site-4au.pages.dev",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
